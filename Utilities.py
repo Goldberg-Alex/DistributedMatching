d2193e737f316e423993ccf3533f97f190864225
lioreldar <eldar.lior@gmail.com>
2020-11-26 13:47:31 +0200

    new files

--- Utilities.py
+++ Utilities.py
@@ -1,11 +1,18 @@
+import copy
 import itertools
 import random
-from typing import List
+from typing import List, Tuple, Set, Any
 
+import matplotlib.pyplot as plt
 import networkx as nx
 
 import Settings
-from AgentBase import AgentBase, MatchingSolution
+from Agent import Agent, MatchingSolution
+
+"""
+a collection of utility methods for working with graphs, 
+you may use these functions freely in the solution
+"""
 
 
 def calculate_agent_matching_value(matching_solution: MatchingSolution, matching_graph) -> int:
@@ -14,7 +21,7 @@ def calculate_agent_matching_value(matching_solution: MatchingSolution, matching
     return score
 
 
-def calc_matching_score(agents: List[AgentBase], matching_graph: nx.Graph) -> float:
+def calc_matching_score(agents: List[Agent], matching_graph: nx.Graph) -> float:
     matching_score_sum = 0.0
 
     for agent in agents:
@@ -23,7 +30,7 @@ def calc_matching_score(agents: List[AgentBase], matching_graph: nx.Graph) -> fl
     return average_matching_score
 
 
-def calculate_average_discrepancy(agents: List[AgentBase]) -> float:
+def calculate_average_discrepancy(agents: List[Agent]) -> float:
     discrepancies_sum = 0.0
     agent_pairs_num = 0
 
@@ -49,6 +56,10 @@ def calculate_discrepancy_score(first_solution: MatchingSolution, second_solutio
     discrepancy = max(len(sorted_first_solution), len(sorted_second_solution)) - len(solutions_intersection)
 
     return discrepancy
+
+
+do_once_full_graph = True
+do_once_subgraph = True
 
 
 def generate_matching_sub_graphs(matching_graph) -> List[nx.Graph]:
@@ -86,6 +97,19 @@ def generate_matching_sub_graphs(matching_graph) -> List[nx.Graph]:
                 validated = False
                 break
 
+    global do_once_subgraph
+    if do_once_subgraph:
+        for sub_graph in sub_graphs:
+            subgraph_copy = copy.deepcopy(sub_graph)
+            if do_once_subgraph:
+                for edge in sub_graph.edges():
+                    edge_weight = sub_graph.get_edge_data(*edge)['weight']
+                    if edge_weight < 1:
+                        subgraph_copy.remove_edge(*edge)
+
+                draw_bipartite_graph(subgraph_copy)
+            do_once_subgraph = False
+
     return sub_graphs
 
 
@@ -97,6 +121,9 @@ def generate_connectivity_graph() -> nx.Graph:
         connectivity_graph = nx.erdos_renyi_graph(n=Settings.NUM_AGENTS, p=Settings.CONNECTIVITY_GRAPH_EDGE_PROPORTION)
         connected = nx.is_connected(connectivity_graph)
 
+    nx.draw(connectivity_graph)
+    plt.show()
+
     return connectivity_graph
 
 
@@ -105,10 +132,25 @@ def generate_matching_graph():
     for _, _, d in matching_graph.edges(data=True):
         d['weight'] = random.randint(*Settings.MATCHING_WEIGHTS_LIMITS)
 
+    global do_once_full_graph
+    if do_once_full_graph:
+        draw_bipartite_graph(matching_graph)
+        do_once_full_graph = False
+
     return matching_graph
 
 
-def is_solved(agents: List[AgentBase], matching_graph: nx.Graph) -> bool:
+def draw_bipartite_graph(matching_graph):
+    l, r = nx.bipartite.sets(matching_graph)
+    pos = {}
+    # Update position for node from each group
+    pos.update((node, (1, index)) for index, node in enumerate(l))
+    pos.update((node, (2, index)) for index, node in enumerate(r))
+    nx.draw(matching_graph, pos=pos)
+    plt.show()
+
+
+def is_solved(agents: List[Agent], matching_graph: nx.Graph) -> bool:
     solutions = [agent.get_solution() for agent in agents]
 
     if not is_valid_matching(matching_graph=matching_graph, solutions=solutions):
@@ -140,3 +182,12 @@ def is_valid_matching(matching_graph: nx.Graph, solutions: List[MatchingSolution
             return False
 
     return True
+
+
+def count_weighted_edges(graph):
+    count = 0
+
+    for edge in graph.edges(data=True):
+        count += (edge[2]['weight'] > 0)
+
+    return count
--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from typing import Dict, List
 import networkx as nx
 
 import Settings
-from AgentBase import Message, AgentID, AgentBase
-from SmartAgent import Agent
+from Agent import Agent
 from Utilities import generate_matching_graph, generate_matching_sub_graphs, generate_connectivity_graph, \
     is_solved
+
+'''
+how to use this file:
+run it.!
+'''
 
 
 def main(message_budget) -> bool:
     # generate matching graph with random weights
-    # random.seed(1)
-
     matching_graph = generate_matching_graph()
 
     sub_graphs = generate_matching_sub_graphs(matching_graph)
@@ -23,28 +25,35 @@ def main(message_budget) -> bool:
     prev_round_messages: Dict[AgentID, List[Message]] = {}
     agents_done = False
 
+    # run untill all agents are done or the message budget is 0
     while curr_message_budget > 0 and not agents_done:
         curr_round_messages: Dict[AgentID, List[Message]] = {}
         agents_done = True
 
+        # iterate over agents, pass them the current messages and get the messages for the next round
         for agent in agents:
-
+            # run an agent step
             new_messages = agent.step(message_budget=curr_message_budget,
                                       messages=prev_round_messages.get(agent.agent_idx, []))
+            # check if the agent is done.
+            # The rounds stop only if all agents are done
             agents_done = agents_done and agent.is_done()
 
+            # save the agent's messages for the next round
             for recipient_idx, message in new_messages.items():
+                # messages that are addressed to an agent which is not a neighbor are discarded
                 if not connectivity_graph.has_edge(agent.agent_idx, recipient_idx):
-                    print(f"Message sent from: agent {agent.agent_idx} to an invalid recipient: agent {recipient_idx}. "
-                          f"Ignoring message.")
                     continue
 
                 if curr_message_budget <= 0:
                     break
+
                 if recipient_idx not in curr_round_messages.keys():
                     curr_round_messages[recipient_idx] = []
-
+                # add the message to the next round's messages
                 curr_round_messages[recipient_idx].append(message)
+
+                # each message decreases the budget by 1
                 curr_message_budget -= 1
 
         prev_round_messages = curr_round_messages
@@ -54,14 +63,11 @@ def main(message_budget) -> bool:
         agent.step(message_budget=curr_message_budget,
                    messages=prev_round_messages.get(agent.agent_idx, []))
 
-    if is_solved(agents=agents, matching_graph=matching_graph):
-        print(f"solved, used messages: {message_budget - curr_message_budget} / {message_budget}.")
-        return True
-
-    return False
+    # check if the agents managed to solve the problem
+    return is_solved(agents=agents, matching_graph=matching_graph)
 
 
-def generate_agents(connectivity_graph: nx.Graph, sub_graphs: List[nx.Graph]) -> List[AgentBase]:
+def generate_agents(connectivity_graph: nx.Graph, sub_graphs: List[nx.Graph]) -> List[Agent]:
     agents = []
     for agent_idx in range(Settings.NUM_AGENTS):
         agents.append(Agent(agent_idx=agent_idx,
@@ -71,13 +77,14 @@ def generate_agents(connectivity_graph: nx.Graph, sub_graphs: List[nx.Graph]) ->
 
 
 if __name__ == '__main__':
+    """this is the main function, it runs the challenge over 3 difficulty levels and prints"""
     if main(Settings.NUM_AGENTS ** 3):
         print("**************EASY test - passed")
 
         if main(Settings.NUM_AGENTS ** 2):
             print("**************MEDIUM test - passed")
 
-            if main(Settings.NUM_AGENTS):
+            if main(Settings.NUM_AGENTS * 2):
                 print("**************HARD test - passed! ")
                 print("Congratulations, you passed the tutorial.")
                 print("Now, prove P != NP")
